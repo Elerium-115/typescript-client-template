6464211f3779a19edef26aa9ab9c6b8ab9a42d10
Serban Stokker <serbanstokker@Stokky-iMac.local>
2024-09-12 21:36:23 +0300

    Initial template with src and static folders

---
+++ src/test-app.ts
@@ -0,0 +1,7 @@
+import {TestModule} from "./test-module.js";
+
+const test: string = 'zzz';
+console.log(`--- test = ${test}`); //// TEST
+
+const testModule = new TestModule();
+console.log(`--- testModule:`, testModule); //// TEST
---
+++ src/test-module.ts
@@ -0,0 +1,3 @@
+class TestModule {};
+
+export {TestModule};
